66c40a16db35e3177737cda9e93989e60ec76bda
Chris <chris@koose.com>
2021-03-18 12:01:25 -0400

    update

--- sample.test.ts
+++ sample.test.ts
@@ -6,13 +6,18 @@ export class Resource {
 
 	async setup() {
 		console.log(`sample.test.ts:Resource:setup: Setup started for ${this.uniqueId}`);
-		const password = "11341134";
+		const password = "<PASSWORD HERE>";
 
 		try {
+			console.log(`sample.test.ts:Resource:setup: Setup 1 ${this.uniqueId}`);
 			this.pgClient = new Client({
 				connectionString: `postgresql://postgres:${password}@localhost:5432/testing`,
 			});
+			console.log(`sample.test.ts:Resource:setup: Setup 2 ${this.uniqueId}`);
+			await new Promise<void>((resolve) => setTimeout(resolve, 1000));
+			console.log(`sample.test.ts:Resource:setup: Setup 3 ${this.uniqueId}`);
 			await this.pgClient.connect();
+			console.log(`sample.test.ts:Resource:setup: Setup 4 ${this.uniqueId}`);
 		} catch (err) {
 			console.error(
 				`sample.test.ts:Resource:setup: Unable to connect to database for ${this.uniqueId}: ${err.toString()}`
@@ -25,7 +30,17 @@ export class Resource {
 	async teardown(): Promise<void> {
 		console.log(`sample.test.ts:Resource:teardown: Teardown started for ${this.uniqueId}`);
 		try {
-			await new Promise<void>((resolve) => setTimeout(resolve, 500));
+			await this.pgClient.query('select * from "test-1"."User"');
+			console.log(`sample.test.ts:Resource:teardown: Teardown 5 for ${this.uniqueId}`);
+			await this.pgClient.query('select * from "test-1"."Organization"');
+			console.log(`sample.test.ts:Resource:teardown: Teardown 4 for ${this.uniqueId}`);
+			await this.pgClient.query('select * from "test-1"."Team"');
+			console.log(`sample.test.ts:Resource:teardown: Teardown 3 for ${this.uniqueId}`);
+			await this.pgClient.query('select * from "test-1"."Device"');
+			console.log(`sample.test.ts:Resource:teardown: Teardown 2 for ${this.uniqueId}`);
+			await this.pgClient.query('select * from "test-1"."RateLimiter"');
+			console.log(`sample.test.ts:Resource:teardown: Teardown 1 for ${this.uniqueId}`);
+			// await new Promise<void>((resolve) => setTimeout(resolve, 500));
 			if (this.pgClient) await this.pgClient.end();
 		} catch (err) {
 			console.error(
@@ -49,6 +64,7 @@ declare global {
 describe("test", () => {
 	let resource: Resource;
 	beforeEach(async () => {
+		console.log("in BeforeEach, allocating resources");
 		resource = new Resource();
 		console.log(`sample.test.ts:beforeEach: Before started for ${resource.uniqueId}`);
 		if (global.nodeGlobal) global.nodeGlobal.resources.push(resource);
