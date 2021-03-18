41743712c638dd359243bce2f331fac59c3a1314
Chris <chris@koose.com>
2021-03-18 12:40:32 -0400

    Added Promise to teardown

--- wallaby.js
+++ wallaby.js
@@ -10,21 +10,24 @@ module.exports = function (wallaby) {
 		teardown: function (wallaby) {
 			console.log(`wallaby.js:teardown: Tearing down ${global.resources.length} setups`);
 			let setupNumber = 1;
-			global.resources.forEach(async (resource) => {
-				try {
-					console.log(
-						`wallaby.js:teardown: #${setupNumber}: Tearing down lost setup #${resource.uniqueId}...`
-					);
-					await resource.teardown();
-					console.log(`wallaby.js:teardown: #${setupNumber}: #${resource.uniqueId} torn down`);
-				} catch (err) {
-					console.error(
-						`wallaby.js:teardown: #${setupNumber}: Failed to tear down setup #${resource.uniqueId}: ${err}`
-					);
-					console.log(err.stack);
-				}
+			return new Promise((resolve) => {
+				global.resources.forEach(async (resource) => {
+					try {
+						console.log(
+							`wallaby.js:teardown: #${setupNumber}: Tearing down lost setup #${resource.uniqueId}...`
+						);
+						await resource.teardown();
+						console.log(`wallaby.js:teardown: #${setupNumber}: #${resource.uniqueId} torn down`);
+					} catch (err) {
+						console.error(
+							`wallaby.js:teardown: #${setupNumber}: Failed to tear down setup #${resource.uniqueId}: ${err}`
+						);
+						console.log(err.stack);
+					}
+				});
+				console.log("wallaby.js:teardown: Teardown complete");
+				resolve();
 			});
-			console.log("wallaby.js:teardown: Teardown complete");
 		},
 	};
 };
